34245fb16df358060fa2119cd7abee27f633983e
Wayfaring-Cognizance <Donte589@yahoo.com>
2020-05-27 12:06:41 -0600

    Update The Willsistance but under construction.py

--- The Willsistance but under construction.py
+++ The Willsistance but under construction.py
@@ -1,71 +1,101 @@
 import random
 
-ten_players_no_special = ['Get Jiggy Pharoh - Bad','Gemini - Bad','After Earth - Bad','Shark Tale Character - Bad','The bird from his new movie - Good','Dr. Hitchens - Good','I,Robot - Good','Independence Day Will - Good','Jim West - Good','Agent J - Good']
-nine_players_no_special = ['Get Jiggy Pharoh - Bad','Gemini - Bad','After Earth - Bad','The bird from his new movie - Good','Dr. Hitchens - Good','I,Robot - Good','Independence Day Will - Good','Jim West - Good','Agent J - Good']
-eight_players_no_special = ['Get Jiggy Pharoh - Bad','Gemini - Bad','After Earth - Bad','Dr. Hitchens - Good','I,Robot - Good','Independence Day Will - Good','Jim West - Good','Agent J - Good']
-seven_players_no_special = ['Get Jiggy Pharoh - Bad','Gemini - Bad','After Earth - Bad','Dr. Hitchens - Good','Independence Day Will - Good','Jim West - Good','Agent J - Good']
-six_players_no_special = ['Get Jiggy Pharoh - Bad','After Earth - Bad','Dr. Hitchens - Good','Independence Day Will - Good','Jim West - Good','Agent J - Good']
-five_players_no_special = ['Get Jiggy Pharoh - Bad','Gemini - Bad','After Earth - Bad','Dr. Hitchens - Good','Jim West - Good','Agent J - Good']
-
-ten_players_merlin = ['Get Jiggy Pharoh - Bad','Gemini - Bad','Deadshot - Bad','Hancock - Bad','Fresh Prince - Good','Dr. Hitchens - Good','I,Robot - Good','Miami Will - Good','Jim West - Good','Agent J - Good']
-nine_players_merlin = ['Get Jiggy Pharoh - Bad','Deadshot - Bad','Hancock - Bad','Fresh Prince - Good','Dr. Hitchens - Good','I,Robot - Good','Miami Will - Good','Jim West - Good','Agent J - Good']
-eight_players_merlin = ['Get Jiggy Pharoh - Bad','Deadshot - Bad','Hancock - Bad','Fresh Prince - Good','Dr. Hitchens - Good','Miami Will - Good','Jim West - Good','Agent J - Good']
-seven_players_merlin = ['Get Jiggy Pharoh - Bad','Deadshot - Bad','Hancock - Bad','Fresh Prince - Good','Miami Will - Good','Jim West - Good','Agent J - Good']
-six_players_merlin = ['Deadshot - Bad','Hancock - Bad','Fresh Prince - Good','Miami Will - Good','Jim West - Good','Agent J - Good']
-five_players_merlin = ['Deadshot - Bad','Hancock - Bad','Fresh Prince - Good','Miami Will - Good','Agent J - Good']
-
-ten_players_merlin_and_mordred = ['Get Jiggy Pharoh - Bad','Will Smith Genie - Bad','Deadshot - Bad','Hancock - Bad','Fresh Prince - Good','Dr. Hitchens - Good','I,Robot - Good','Miami Will - Good','Jim West - Good','Agent J - Good']
-nine_players_merlin_and_mordred = ['Deadshot - Bad','Will Smith Genie - Bad','Hancock - Bad','Fresh Prince - Good','Dr. Hitchens - Good','I,Robot - Good','Miami Will - Good','Jim West - Good','Agent J - Good']
-eight_players_merlin_and_mordred = ['Deadshot - Bad','Will Smith Genie - Bad','Hancock - Bad','Fresh Prince - Good','Dr. Hitchens - Good','Miami Will - Good','Jim West - Good','Agent J - Good']
-seven_players_merlin_and_mordred = ['Deadshot - Bad','Will Smith Genie - Bad','Hancock - Bad','Fresh Prince - Good','Miami Will - Good','Jim West - Good','Agent J - Good']
-six_players_merlin_and_mordred = ['Will Smith Genie - Bad','Hancock - Bad','Fresh Prince - Good','Miami Will - Good','Jim West - Good','Agent J - Good']
-five_players_merlin_and_mordred = ['Will Smith Genie - Bad','Hancock - Bad','Fresh Prince - Good','Miami Will - Good','Agent J - Good']
-
-ten_players_all_roles = ['Will Smith on Youtube - Bad','Will Smith Genie - Bad','Deadshot - Bad','Hancock - Bad','Fresh Prince - Good','Dr. Hitchens - Good','I,Robot - Good','Miami Will - Good','Jim West - Good','Agent J - Good']
-nine_players_all_roles = ['Will Smith on Youtube - Bad','Will Smith Genie - Bad','Hancock - Bad','Fresh Prince - Good','Dr. Hitchens - Good','I,Robot - Good','Miami Will - Good','Jim West - Good','Agent J - Good']
-eight_players_all_roles = ['Will Smith on Youtube - Bad','Will Smith Genie - Bad','Hancock - Bad','Fresh Prince - Good','Dr. Hitchens - Good','Miami Will - Good','Jim West - Good','Agent J - Good']
-seven_players_all_roles = ['Will Smith on Youtube - Bad','Will Smith Genie - Bad','Hancock - Bad','Fresh Prince - Good','Miami Will - Good','Jim West - Good','Agent J - Good']
-
+Good_Roles = ['The bird from his new movie - Good','Dr. Hitchens - Good','I,Robot - Good','Independence Day Will - Good','Jim West - Good','Agent J - Good']
+Bad_Roles = ['Get Jiggy Pharoh - Bad','Gemini - Bad','After Earth - Bad', 'Shark Tale Character - Bad']
 
 def game_info():
     global player_list
+    global special_roles
     players = int(input('How many people are playing? '))
-    roles = int(input('''Please select an option:
+    special_roles = int(input('''Please select an option:
                   1. Merlin, Morgana, Percival
                   2. Merlin, Morgana, Percival, Mordred
                   3. All roles
                   4. No special roles
                   '''))
-    player_one = input('Player One: ')
-    player_two = input('Player Two: ')
-    player_three = input('Player Three: ')
-    player_four = input('Player Four: ')
-    player_five = input('Player Five: ')
-    player_six = input('Player Six: ')
-    player_seven = input('Player Seven: ')
-    player_eight = input('Player Eight: ')
-    player_nine = input('Player Nine: ')
-    player_ten = input('Player Ten: ')
+
     if players == 10:
+        player_one = input('Player One: ')
+        player_two = input('Player Two: ')
+        player_three = input('Player Three: ')
+        player_four = input('Player Four: ')
+        player_five = input('Player Five: ')
+        player_six = input('Player Six: ')
+        player_seven = input('Player Seven: ')
+        player_eight = input('Player Eight: ')
+        player_nine = input('Player Nine: ')
+        player_ten = input('Player Ten: ')
         player_list = [player_one, player_two, player_three, player_four, player_five, player_six, player_seven, player_eight, player_nine, player_ten]
     elif players == 9:
+        Bad_Roles.remove('Gemini - Bad')
+        player_one = input('Player One: ')
+        player_two = input('Player Two: ')
+        player_three = input('Player Three: ')
+        player_four = input('Player Four: ')
+        player_five = input('Player Five: ')
+        player_six = input('Player Six: ')
+        player_seven = input('Player Seven: ')
+        player_eight = input('Player Eight: ')
+        player_nine = input('Player Nine: ')
         player_list = [player_one, player_two, player_three, player_four, player_five, player_six, player_seven, player_eight, player_nine]
     elif players == 8:
+        Bad_Roles.remove('Gemini - Bad')
+        Good_Roles.remove('The bird from his new movie - Good')
+        player_one = input('Player One: ')
+        player_two = input('Player Two: ')
+        player_three = input('Player Three: ')
+        player_four = input('Player Four: ')
+        player_five = input('Player Five: ')
+        player_six = input('Player Six: ')
+        player_seven = input('Player Seven: ')
+        player_eight = input('Player Eight: ')
         player_list = [player_one, player_two, player_three, player_four, player_five, player_six, player_seven, player_eight]
     elif players == 7:
+        Bad_Roles.remove('Gemini - Bad')
+        Good_Roles.remove('I,Robot - Good')
+        Good_Roles.remove('The bird from his new movie - Good')
+        player_one = input('Player One: ')
+        player_two = input('Player Two: ')
+        player_three = input('Player Three: ')
+        player_four = input('Player Four: ')
+        player_five = input('Player Five: ')
+        player_six = input('Player Six: ')
+        player_seven = input('Player Seven: ')
         player_list = [player_one, player_two, player_three, player_four, player_five, player_six, player_seven]
     elif players == 6:
+        Bad_Roles.remove('After Earth - Bad')
+        Bad_Roles.remove('Gemini - Bad')
+        Good_Roles.remove('I,Robot - Good')
+        Good_Roles.remove('The bird from his new movie - Good')
+        player_one = input('Player One: ')
+        player_two = input('Player Two: ')
+        player_three = input('Player Three: ')
+        player_four = input('Player Four: ')
+        player_five = input('Player Five: ')
+        player_six = input('Player Six: ')
         player_list = [player_one, player_two, player_three, player_four, player_five, player_six]
     else:
+        Bad_Roles.remove('After Earth - Bad')
+        Bad_Roles.remove('Gemini - Bad')
+        Good_Roles.remove('I,Robot - Good')
+        Good_Roles.remove('The bird from his new movie - Good')
+        Good_Roles.remove('Dr. Hitchens - Good')
+        player_one = input('Player One: ')
+        player_two = input('Player Two: ')
+        player_three = input('Player Three: ')
+        player_four = input('Player Four: ')
+        player_five = input('Player Five: ')
         player_list = [player_one, player_two, player_three, player_four, player_five]
 
 
 def player_assignments():
-    if players == 10 and roles
-    random.shuffle(ten_players_no_special)
-    assignments = {ten_players_no_special[i]: player_list[i] for i in range(len(ten_players_no_special))}
-    assignments = [(k, v) for k, v in assignments.items()]
-    print (str(assignments))
+    if special_roles == 4:
+        roles =  (Good_Roles + Bad_Roles)
+        random.shuffle(roles)
+        assignments = zip(roles, player_list)
+        money_shot = list(assignments)
+        print(list(money_shot))
 
 game_info ()
 player_assignments ()
+
